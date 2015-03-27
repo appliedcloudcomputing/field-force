68d264ff4d387b7d31badd469f606dae954d9a51
Arjun Kori <arjun@appliedcloudcomputing.com>
2015-03-27 15:15:41 +0530

    User List Page Updated

--- ff-cloud/cloud/main.js
+++ ff-cloud/cloud/main.js
@@ -100,26 +100,3 @@ Parse.Cloud.define('saveJob', function(req, res) {
 	} 
 });
 
-
-/*-----------------------------------------------CHANGE PASSWORD-----------------------------------------------*/
-
-Parse.Cloud.define("changePassword", function(request, response) 
-{
-  var query = new Parse.Query(Parse.User);
-  query.equalTo("username", request.params.username);
-  query.find({
-               success: function(results) 
-                {
-      				var sum = 0;
-                    for (var i = 0; i < results.length; ++i) 
-                         {
-                          sum += results[i].get("stars");
-                         }
-                     response.success(sum / results.length);
-                },
-    			error: function() 
-    			{
-                   response.error("movie lookup failed");
-                }
-            });
-});
