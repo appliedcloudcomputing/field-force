d1e7d1970b20f0c7a16df0bf4d62ba1a131ae430
arjunkori <arjun@appliedcloudcomputing.com>
2015-06-01 17:57:37 +0530

    image upload code

--- ff-web/routes/profile.js
+++ ff-web/routes/profile.js
@@ -67,6 +67,50 @@ router.post('/update', function(req, res, next) {
   }  
 });
 
+router.post('/uploadImage',function(req,res){
+  console.log("Upload image called");
+  console.log("File : "+req.body.myPhoto);
+  
+  var file = req.body.myPhoto;
+  var name =file.name;
+  console.log("line 76");
+  var parseFile = new Parse.File(name,{base64:file});
+
+  parseFile.save().then
+         (
+           function() 
+           {
+            Parse.Cloud.useMasterKey();
+            var query = new Parse.Query(Parse.User);
+            query.equalTo("objectId","S8qZJM7XIA");
+            query.find({
+            success: function(results) 
+            {
+            results.set("profileImg",parseFile);
+            results.save(null, {
+            success: function(results) {
+              params.success(Response.UpdateSuccess);
+            },
+            error: function(gameScore, error) {
+            console.log('Failed to create new object, with error code: ' + error.description);
+          }
+     });
+  }
+
+})
+               
+           }, 
+           function(error) 
+           {
+             console.log("error"+error);
+           }
+         );
+    
+    
+
+});
+
+
 
 module.exports = router;
 
